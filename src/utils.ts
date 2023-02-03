b5a5aad06bff74bfbd98c327662de89052e26a26
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-02-02 23:54:04 -0300

    Return previous changes

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import md5 from "md5";
 
 const timestamp = Date.now();
-const BASE_URL = import.meta.env.VITE_MARVEL_BASE_URL;
-const PUBLIC_KEY = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
-const PRIVATE_KEY = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
+const BASE_URL = "https://gateway.marvel.com:443/v1/public";
+const PUBLIC_KEY = "53bcde9ea27ef56a6c7504ad9db3c768";
+const PRIVATE_KEY = "bc08f8ecce6343e726fcdff6219387d09f38251d";
 
 const hash = md5(String(timestamp) + PRIVATE_KEY + PUBLIC_KEY);
 
