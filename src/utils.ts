9642aa366c7572c9af7c3b87b76a4ecd32eee8de
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-02-02 23:35:12 -0300

    Change API key to another place

--- src/utils.ts
+++ src/utils.ts
@@ -2,15 +2,14 @@ import axios from "axios";
 import md5 from "md5";
 
 const timestamp = Date.now();
-const publicKey = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
-const privateKey = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
+const BASE_URL = "http://gateway.marvel.com/v1/public";
+const PUBLIC_KEY = "53bcde9ea27ef56a6c7504ad9db3c768";
+const PRIVATE_KEY = "bc08f8ecce6343e726fcdff6219387d09f38251d";
 
-const hash = md5(String(timestamp) + privateKey + publicKey);
+const hash = md5(String(timestamp) + PRIVATE_KEY + PUBLIC_KEY);
 
 export const get = (endpoint: string, offset?: number) => {
-  const url = `${
-    import.meta.env.VITE_MARVEL_BASE_URL
-  }${endpoint}?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+  const url = `${BASE_URL}${endpoint}?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`;
 
   if (offset) {
     return axios.get(url + `&offset=${offset}`).then((resp) => resp.data.data);
