87f36bffe54a8590c34b89dc3965be6447d906ae
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-02-26 16:47:59 -0300

    Refactor get request API

--- src/utils.ts
+++ src/utils.ts
@@ -9,11 +9,14 @@ const PRIVATE_KEY = "bc08f8ecce6343e726fcdff6219387d09f38251d";
 const hash = md5(String(timestamp) + PRIVATE_KEY + PUBLIC_KEY);
 
 export const get = (endpoint: string, offset?: number) => {
-  const url = `${BASE_URL}${endpoint}?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`;
+  const url = `${BASE_URL}${endpoint}`;
 
-  if (offset) {
-    return axios.get(url + `&offset=${offset}`).then((resp) => resp.data.data);
-  } else {
-    return axios.get(url).then((resp) => resp.data.data);
-  }
+  const customParams = {
+    offset: offset,
+    ts: timestamp,
+    apikey: PUBLIC_KEY,
+    hash: hash,
+  };
+
+  return axios.get(url, { params: customParams }).then((resp) => resp.data.data);  
 };
