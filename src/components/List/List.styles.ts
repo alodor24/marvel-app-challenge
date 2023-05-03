fb45037401e3b5b774dda50e01ffae3075fe7c0f
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-05-03 09:22:41 -0300

    Adjust styles columns on list component

--- src/components/List/List.styles.ts
+++ src/components/List/List.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
   gap: 2rem;
   padding: 5rem 0;
 `;
